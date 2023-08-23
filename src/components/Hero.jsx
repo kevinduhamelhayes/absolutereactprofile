a8f693d646fa3cd466c48ea3a6974438a82e5651
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-08-23 03:54:28 -0300

    hi navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import "./styles/Hero.css"
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <>
+      <div>
+        <figure>
+          <img src="" alt="" />
+        </figure>
+        <div className="hero-tittle">Ableton</div>
+      </div>
+      <div className="hero-subtittle">
+        <h2 className="h2-black">
+          We make <a href="#live">Live</a> , <a href="#push">Push</a> and{" "}
+          <a href="#link">Link</a> unique software and hardware for music
+          creation and performance. With these products, our community of users
+          creates amazing things
+        </h2>
+      </div>
+    </>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,75 @@
-
-
+import "./styles/Navbar.css"
+import { FaPlus } from "react-icons/fa"
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="navbar">
+      <div className="navbar-sticky">
+        <div className="navbar__logo">
+          <div className="logo1">
+            <div className="col"></div>
+            <div className="col"></div>
+            <div className="col"></div>
+            <div className="col"></div>
+          </div>
+          <div className="logo2">
+            <div className="col"></div>
+            <div className="col"></div>
+            <div className="col"></div>
+            <div className="col"></div>
+          </div>
+        </div>
+        <div className="container_links_navbar">
+        <div className="navbar_links_left">
+          <ul className="ul-navbar">
+            <li className="li-navbar">
+              <a href="#live">Live</a>
+            </li>
+            <li className="li-navbar">
+              <a href="#push">Push</a>
+            </li>
+            <li className="li-navbar">
+              <a href="#link">Link</a>
+            </li>
+            <li className="li-navbar">
+              <a href="#shop">Shop</a>
+            </li>
+            <li className="li-navbar">
+              <a href="#packs">Packs</a>
+            </li>
+            <li className="li-navbar">
+              <a href="#help">Help</a>
+            </li>
+            <li className="li-navbar">
+              <a href="#more">More <FaPlus/></a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar_links_right">
+        <li className="li-navbar" >
+            <a id="Try" href="#Try">Try Live for Free</a>
+          </li>
+
+          <li className="li-navbar">
+            <a href="#login">Login</a>
+          </li>
+          <li className="li-navbar">
+            <a href="#signup">Sign Up</a>
+          </li>
+
+        </div>
+      </div>
+      </div>
+      <div className="navbar-top">
+        <ul className="navbar-top1">
+          <li className="li-navbar">
+            <a href="#about">About</a>
+          </li>
+          <li className="li-navbar">
+            <a href="Jobs">Jobs</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
