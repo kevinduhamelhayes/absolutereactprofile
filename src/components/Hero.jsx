69606b37d2b834231d67b62ea604b0b76a18bbe3
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-08-23 04:28:46 -0300

    hola git

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,19 +3,27 @@ import "./styles/Hero.css"
 const Hero = () => {
   return (
     <>
-      <div>
-        <figure>
-          <img src="" alt="" />
+      <div className="hero-top">
+        <figure className="hero-figure">
+          <img className="hero-img" src="./public/hero.jpeg" alt="" />
         </figure>
         <div className="hero-tittle">Ableton</div>
       </div>
-      <div className="hero-subtittle">
-        <h2 className="h2-black">
-          We make <a href="#live">Live</a> , <a href="#push">Push</a> and{" "}
-          <a href="#link">Link</a> unique software and hardware for music
-          creation and performance. With these products, our community of users
-          creates amazing things
-        </h2>
+      <div className="hero-bottom">
+        <div className="hero-subtittle">
+          <h2 className="h2-black">
+            We make <a href="#live">Live</a> , <a href="#push">Push</a> and{" "}
+            <a href="#link">Link</a> unique software and hardware for music
+            creation and performance. With these products, our community of
+            users creates amazing things
+          </h2>
+          <p className="hero-paragraf">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia
+            debitis necessitatibus provident iste cupiditate harum animi vel,
+            corrupti consequuntur amet molestias non, rem unde velit quod error
+            saepe! Illo, perspiciatis omnis. Repellendus.
+          </p>
+        </div>
       </div>
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,31 @@
 import "./styles/Navbar.css"
 import { FaPlus } from "react-icons/fa"
+import { useState, useEffect } from "react"
+
+
+
 const Navbar = () => {
+  const [showTopNav, setShowTopNav] = useState(true);
+  useEffect(() => {
+    let lastScrollTop = 0;
+  
+    const handleScroll = () => {
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      if (scrollTop > lastScrollTop){
+        setShowTopNav(false); // oculta navbar-top
+      } else {
+        setShowTopNav(true); // muestra navbar-top
+      }
+      lastScrollTop = scrollTop;
+    }
+  
+    window.addEventListener("scroll", handleScroll);
+  
+    // Limpia el event listener al desmontar el componente
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }, []);
   return (
     <nav className="navbar">
       <div className="navbar-sticky">
@@ -19,47 +44,50 @@ const Navbar = () => {
           </div>
         </div>
         <div className="container_links_navbar">
-        <div className="navbar_links_left">
-          <ul className="ul-navbar">
+          <div className="navbar_links_left">
+            <ul className="ul-navbar">
+              <li className="li-navbar">
+                <a href="#live">Live</a>
+              </li>
+              <li className="li-navbar">
+                <a href="#push">Push</a>
+              </li>
+              <li className="li-navbar">
+                <a href="#link">Link</a>
+              </li>
+              <li className="li-navbar">
+                <a href="#shop">Shop</a>
+              </li>
+              <li className="li-navbar">
+                <a href="#packs">Packs</a>
+              </li>
+              <li className="li-navbar">
+                <a href="#help">Help</a>
+              </li>
+              <li className="li-navbar">
+                <a href="#more">
+                  More <FaPlus />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar_links_right">
             <li className="li-navbar">
-              <a href="#live">Live</a>
+              <a id="Try" href="#Try">
+                Try Live for Free
+              </a>
             </li>
-            <li className="li-navbar">
-              <a href="#push">Push</a>
-            </li>
-            <li className="li-navbar">
-              <a href="#link">Link</a>
-            </li>
-            <li className="li-navbar">
-              <a href="#shop">Shop</a>
-            </li>
-            <li className="li-navbar">
-              <a href="#packs">Packs</a>
-            </li>
-            <li className="li-navbar">
-              <a href="#help">Help</a>
-            </li>
-            <li className="li-navbar">
-              <a href="#more">More <FaPlus/></a>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar_links_right">
-        <li className="li-navbar" >
-            <a id="Try" href="#Try">Try Live for Free</a>
-          </li>
 
-          <li className="li-navbar">
-            <a href="#login">Login</a>
-          </li>
-          <li className="li-navbar">
-            <a href="#signup">Sign Up</a>
-          </li>
-
+            <li className="li-navbar">
+              <a href="#login">Login</a>
+            </li>
+            <li className="li-navbar">
+              <a href="#signup">Sign Up</a>
+            </li>
+          </div>
         </div>
       </div>
-      </div>
-      <div className="navbar-top">
+      <div className={`navbar-top ${!showTopNav ? 'hidden' : ''}`}>
         <ul className="navbar-top1">
           <li className="li-navbar">
             <a href="#about">About</a>
