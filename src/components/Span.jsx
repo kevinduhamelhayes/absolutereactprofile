2660c43cc45dbf2e5c2af04598c1fc90e7c6392a
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-08-28 01:36:51 -0300

    commit

--- src/components/Span.jsx
+++ src/components/Span.jsx
@@ -2,16 +2,18 @@ import "./styles/Span.css"
 
 const Span = () => {
   return (
-    <div className="Span-container">
-      <div className="Span-img">
-        <figure className="Span-figure1">
-          <img className="Span-img1" src="./public/span1.jpeg" alt="" />
-        </figure>
-        <figure className="Span-figure2">
-          <img className="Span-img2" src="./public/span2.jpeg" alt="" />
-        </figure>
+    <div className="Span-container-superior">
+      <div className="Span-container">
+        <div className="Span-img">
+          <figure className="Span-figure1">
+            <img className="Span-img1" src="./public/span1.jpeg" alt="" />
+          </figure>
+          <figure className="Span-figure2">
+            <img className="Span-img2" src="./public/span2.jpeg" alt="" />
+          </figure>
+        </div>
+        <div className="Span-yellow"></div>
       </div>
-      <div className="Span-yellow"></div>
     </div>
   )
 }
