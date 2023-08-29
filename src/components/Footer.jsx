6512c4e02daa31e451e5042f921a1d5f244fb418
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-08-29 18:25:21 -0300

    second commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,14 @@
 import "./styles/Footer.css"
-import { FaLinkedin, FaReddit, FaYoutube, FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa"
+import {
+  FaLinkedin,
+  //FaReddit,
+  //FaYoutube,
+  FaFacebook,
+  //FaTwitter,
+  FaInstagram,
+  FaGithub,
+  FaHeart,
+} from "react-icons/fa"
 
 const Footer = () => {
   return (
@@ -8,12 +17,13 @@ const Footer = () => {
         <div className="footer-top">
           <div className="footer-top-left">
             <div className="footer-top-left-title">
-              <h1>Ableton</h1>
+              <h1>Kevin Duhamel Hayes</h1>
             </div>
             <div className="footer-top-left-subtitle">
               <a className="a-footer" href="#live">
-                Live
+                Im a developer in continius learning
               </a>
+              {/*
               <a className="a-footer" href="#push">
                 Push
               </a>
@@ -32,11 +42,16 @@ const Footer = () => {
               <a className="a-footer" href="#more">
                 More
               </a>
+              */}
             </div>
           </div>
           <div className="footer-top-right">
             <div className="footer-top-right-title">Legal Info</div>
             <div className="footer-top-right-subtitle">
+              <a className="a-footer" href="#agreement">
+                This landing page is a crated by Kevin Duhamel for you <FaHeart color="#FF0000" size={20} />
+              </a>
+              {/*
               <a className="a-footer" href="#privacy">
                 Privacy policy
               </a>
@@ -52,50 +67,58 @@ const Footer = () => {
               <a className="a-footer" href="#contact">
                 Contact us
               </a>
+              */}
             </div>
           </div>
         </div>
         <div className="footer-bottom">
-          <div className="footer-bottom-left">
-            <div className="footer-bottom-left-title">
-              Subscribe to our newsletter
-            </div>
-            <div className="footer-bottom-left-subtitle">
-              Enter your email address to stay up to date with the latest
-              offers, tutorials, downloads, surveys and more.
-            </div>
-            <div className="footer-bottom-left-input">
-              <input
-                className="footer-bottom-left-input-text"
-                type="text"
-                placeholder="Enter your email address"
-              />
-              <button className="footer-bottom-left-input-button">
-                Sign up
-              </button>
-            </div>
-          </div>
+        <div className="footer-bottom">
+    <div className="footer-bottom-left">
+        <button 
+            className="footer-bottom-left-input-button"
+            onClick={() => window.location.href = "mailto:kevinduhamelh@gmail.com"}
+        >
+            Contact Me
+        </button>
+    </div>
+</div>
+
           <div className="footer-bottom-right">
-            <div className="footer-bottom-right-title">Follow Ableton</div>
+            <div className="footer-bottom-right-title">Follow Me</div>
             <div className="footer-bottom-right-subtitle">
               <a className="a-footer" href="#facebook">
-                <i className="fab fa-facebook-f"><FaFacebook color="#435998" size={40}/></i>
+                <i className="fab fa-facebook-f">
+                  <FaFacebook color="#435998" size={40} />
+                </i>
               </a>
+              {/*
               <a className="a-footer" href="#twitter">
                 <i className="fab fa-twitter"><FaTwitter color="#49AAEA" size={40}/></i>
               </a>
+        
               <a className="a-footer" href="#youtube">
                 <i className="fab fa-youtube"> <FaYoutube color="#CA1C25" size={40}/></i>
               </a>
+*/}
               <a className="a-footer" href="#instagram">
-                <i className="fab fa-instagram"><FaInstagram color="#FF891E" size={40}/></i>
+                <i className="fab fa-instagram">
+                  <FaGithub color="#9733F7" size={40} />
+                </i>
               </a>
+              <a className="a-footer" href="#instagram">
+                <i className="fab fa-instagram">
+                  <FaInstagram color="#FF891E" size={40} />
+                </i>
+              </a>
+
               <a className="a-footer" href="#linkedin">
                 <FaLinkedin color="#0A66C2" size={40} />
               </a>
+              {/*
               <a className="a-footer" href="#reddit">
                 <i className="fab fa-reddit-alien"><FaReddit color="#FF4500" size={40}/></i>
-              </a>
+              </a> 
+              */}
               <a className="a-footer" href="#soundcloud">
                 <i className="fab fa-soundcloud"></i>
               </a>
